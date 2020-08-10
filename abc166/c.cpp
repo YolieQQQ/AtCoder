a3022756180807f25fc880aeadfb876c203865e6
Yolie <yolie@ezff.dev>
2020-08-10 12:55:45 +0900

    update

--- abc166/c.cpp
+++ abc166/c.cpp
@@ -27,59 +27,41 @@ void print(vector<vector<T>> &df) {
 }
 #define rep(i, n) for (int i = 0, i##_len = (n); i < i##_len; ++i)
 #define xrep(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define zrep(i, a, b, c) for (int i = int(a); i < int(b); i+=int(c))
 using ll = long long;
 const int INF = 1001001001;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> h(n+1);
-    rep(i, n) cin >> h[i+1];
+    int N, M;
+    cin >> N >> M;
+    vector<int> H(N);
 
-    vector<vector<int>> A2B(n+1);
-    vector<vector<int>> B2A(n+1);
-    
+    rep(i, N) cin >> H[i];
 
-    rep(i, m) {
+    vector<vector<int>> to(N);
+    rep(i, M) {
         int a, b;
         cin >> a >> b;
-        A2B[a].push_back(b);
-        B2A[b].push_back(a);
+        --a; --b;
+        to[a].push_back(b);
+        to[b].push_back(a);
     }
-    int cnt = 0;
-    xrep(t, 1, n+1) {
-        int myTall = h[t];
-        vector<int> neighbors = A2B[t];
-        int tallcnt = 0;
-        for(auto neighbor: neighbors) {
-            int neighborTall = h[neighbor];
-            if(neighborTall <= myTall) {
-                tallcnt++;
+    vector<int> OK;
+    int ans = 0;
+    rep(i, N) {
+        int my_height = H[i];
+        int cnt = 0;
+        for(auto v: to[i]) {
+            if(my_height > H[v]) {
+                cnt++;
             }
         }
-        if (tallcnt==neighbors.size() && tallcnt!=0) {
-            ++cnt;
+        if(cnt == to[i].size()) {
+            ans++;
+            OK.push_back(i);
         }
     }
 
-
-    xrep(t, 1, n+1) {
-        int myTall = h[t];
-        vector<int> neighbors = B2A[t];
-        int tallcnt = 0;
-        for(auto neighbor: neighbors) {
-            int neighborTall = h[neighbor];
-            if(neighborTall <= myTall) {
-                tallcnt++;
-            }
-        }
-        if (tallcnt==neighbors.size() && tallcnt!=0) {
-            ++cnt;
-        }
-
-    }
-    print(cnt);
-
-
+    print(ans);
     return 0;
 }
--- abc166/c.py
+++ abc166/c.py
@@ -1,29 +1,31 @@
+from fractions import gcd
+from collections import Counter, deque, defaultdict
+from heapq import heappush, heappop, heappushpop, heapify, heapreplace, merge
+from bisect import bisect_left, bisect_right, bisect, insort_left, insort_right, insort
+from itertools import product, combinations,permutations
+from copy import deepcopy, copy
+from math import sqrt, log10
+import sys
+sys.setrecursionlimit(4100000)
 
-from copy import deepcopy
 
 
 if __name__ == '__main__':
     N, M = map(int, input().split())
-    H = [0] + list(map(int, input().split()))
-    AB = [[]]*(N+1)
-    for _ in range(M):
-        ab = deepcopy(list(map(int, input().split())))
-        AB[ab[0]].append(ab[1])
-    cnt = 0
-    print(AB)
-    for i in range(1, N+1):
-        can_go = AB[i]
-        height = H[i]
-        n = 0
-        for c in can_go:
-            if height > H[c]:
-                n += 1
-            else:
-                break
-        if n==len(can_go):
-            cnt += 1
+    H = list(map(int, input().split()))
+    AB = []
+    for i in range(M):
+        ab = list(map(int, input().split())
+        AB.append(ab)
 
-    print(cnt)
+    for i in range(N):
+        for j in range(N):
+            if i==j: continue
+            if H[i]>H[j]:
+                
+
+
+
     
 
 
